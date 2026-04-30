44ce8cc4f482b4c9c32ca352bc4162f2a60a74b5
abidahakime <abidastudy@gmail.com>
2026-04-30 17:08:20 +0430

    ver orders route with Promise params type

---
+++ my-app/app/(public)/page.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import PriceSection from "@/components/PriceSection";
+import Trust from "@/components/Trust";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <PriceSection />
+      <Trust />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
